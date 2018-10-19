25ee2587701532c3a0c93deff167e42d9a4f469d
Daniel Oliveira <danieliver2011@gmail.com>
2018-10-19 11:22:06 -0300

    criando uma nova rota

--- start/routes.js
+++ start/routes.js
@@ -7,6 +7,8 @@ Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
 
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
 Route.get('/departaments', 'StoreController.getDepartments')
 Route.get('/stores/:departament', 'StoreController.getStoresByDepartaments')
 Route.get('/Stores/:id', 'StoresController.getStoreById')
