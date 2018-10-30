b0f23c9d4c3389bda38259cca2ab26b1ee050768
Daniel Oliveira <danieliver2011@gmail.com>
2018-10-30 11:01:49 -0300

    adicionando mais opções no controller de email

--- app/Controllers/Http/MailController.js
+++ app/Controllers/Http/MailController.js
@@ -3,13 +3,13 @@
 class MailController {
     async sendMail({ request, response}) {
         const Mail = use('Mail')
-        const data = request.only(["nome", "email", "mensagem", "destinatario"])
+        const data = request.only(["nome", "email", "mensagem", "destinatario", "assunto"])
         try {
             await Mail.send('emails.contact', data, (message) => {
                 message
                     .to('suporte@dmxweb.com.br')
                     .from(data.destinatario)
-                    .subject('Contato')
+                    .subject(data.assunto)
             })
             response.json({data})
             
